9268b10eeeb4bd46b74ff7e7114b33b2f9fd6370
Parn Boonyamanee <parnpresso@gmail.com>
2021-03-05 15:47:55 +0700

    Add route and DB compose

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-const fastify = require('fastify')
+const fastify = require('fastify');
 
 function build(opts={}) {
-  const app = fastify(opts)
+  const app = fastify(opts);
+
   app.get('/', async function (request, reply) {
-    return { hello: 'world' }
-  })
+    return { hello: 'world' };
+  });
+  app.get('/islands', async (request, reply) => {
+    return {};
+  });
 
   return app
 }
--- src/app.test.js
+++ src/app.test.js
@@ -12,3 +12,16 @@ describe('Endpoint: /', () => {
     expect(response.body).toEqual(JSON.stringify({ "hello": "world" }));
   });
 });
+
+describe('Endpoint: /islands', () => {
+  const app = build();
+
+  it('should response a islands data', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/islands'
+    })
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(JSON.stringify([{ "name": "Phi Phi Island" }]));
+  });
+});
